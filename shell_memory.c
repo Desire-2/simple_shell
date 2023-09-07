290d217b1dddb5420655f9e5004238e9fa617aa4
root <Desire-2>
2023-09-07 09:53:57 -0700

    Implemented string copy, string duplication, string printing, and character printing functions with error handling and buffer management.

---
+++ shell_memory.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * _str_length - Function that returns the length of a string
+ * @s: The string whose length to be checked
+ *
+ * Return: The integer length of string
+ */
+
+int _str_length(char *s)
+{
+	int n = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		n++;
+	return (n);
+
+}
+
+
+/**
+ * _str_cmp_two - Function performs lexicogarphic comparison of two strings.
+ * @s1: The first string
+ * @s2: The next string
+ *
+ * Return: If s1 < s2 negative , if s1 > s2 positive, if s1 == s2 zero
+ */
+
+int _str_cmp_two(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+
+}
+
+
+/**
+ * starts_with_node - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
+
+char *starts_with_node(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+
+/**
+ * _str_cat - Function that concatenates two strings
+ * @dest: Buffer destination
+ * @src: Buffer source
+ *
+ * Return: pointer to buffer destination
+ */
+
+char *_str_cat(char *dest, char *src)
+{
+	char *rtn = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (rtn);
+
+}
---
+++ string_functions.c
@@ -0,0 +1,93 @@
+#include "main.h"
+/**
+ * _str_cpy - Function that copies a string
+ * @dest: String destination
+ * @src: The source string
+ *
+ * Return: Pointer to destination
+ */
+
+char *_str_cpy(char *dest, char *src)
+{
+	int n = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[n])
+	{
+		dest[n] = src[n];
+		n++;
+	}
+	dest[n] = 0;
+	return (dest);
+}
+
+
+/**
+ * _str_duplicate - Functin that duplicates a string
+ * @str: The string to be duplicated
+ *
+ * Return: Pointer to the duplicated string
+ */
+
+char *_str_duplicate(const char *str)
+{
+    char *rtn;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		len++;
+	rtn = malloc(sizeof(char) * (len + 1));
+	if (!rtn)
+		return (NULL);
+	for (len++; len--;)
+		ret[len] = *--str;
+	return (rtn);
+}
+
+
+/**
+ * _puts_str - Function to print an input string
+ * @str: The string to print
+ *
+ * Return: Nothing
+ */
+
+void _puts_str(char *str)
+{
+	int n = 0;
+
+	if (!str)
+		return;
+	while (str[n] != '\0')
+	{
+		_put_char_std(str[n]);
+		n++;
+	}
+}
+
+
+/**
+ * _put_char_std - Function writes the character c to stdout
+ * @c: The character to be printed
+ *
+ * Return: 1 On success.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _put_char_std(char c)
+{
+    static char buffer[WRITE_BUF_SIZE];
+	static int n;
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buffer, n);
+		n = 0;
+	}
+	if (c != BUF_FLUSH)
+		buffer[n++] = c;
+	return (1);
+}
